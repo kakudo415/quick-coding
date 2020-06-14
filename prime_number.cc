c567a4cced71a34b911f8ea36167f46bb83e6c61
Kakudo <kakudo415@gmail.com>
2020-06-14 23:08:19 +0900

    + エラトステネスの篩

---
+++ prime_number.cc
@@ -0,0 +1,14 @@
+// Sieve of Eratosthenes O(nloglogn)
+vector<bool> sieve(int n) {
+  vector<bool> is_prime(n + 1, true);
+  is_prime[0] = false;
+  is_prime[1] = false;
+  for (int i = 2; i * i <= n; i++) {
+    if (is_prime[i]) {
+      for (int j = i * 2; j <= n; j += i) {
+        is_prime[j] = false;
+      }
+    }
+  }
+  return is_prime;
+}
