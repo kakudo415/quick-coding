6262d7e45d2272b7a711342de827918ee1580432
kakudo415 <kakudo415@gmail.com>
2022-03-20 15:24:27 +0900

    マクロを追加し、solve関数を切り出し。

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <queue>
@@ -12,6 +13,16 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
+#define FOR(i, b, e) for (int i = b; i < e; i++)
+#define rep(i, e) for (int i = 0; i < e; i++)
+
 const ld pi = 3.141592653589793;
 
-int main() {}
+void solve() {}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  cout << fixed << setprecision(16);
+  solve();
+}
